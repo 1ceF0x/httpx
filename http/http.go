eb404e635438a0f9a9bdc772c553357105a15287
V <Vsir@88.com>
2022-11-17 18:20:42 +0700

    1.增加Client默认配置及自定义配置，使用默认配置无需进行初始化Client 2.优化代理配置，可灵活配置代理

--- http/http.go
+++ http/http.go
@@ -2,16 +2,13 @@ package httpx
 
 import (
 	"crypto/tls"
+	"fmt"
 	"github.com/valyala/fasthttp"
 	"github.com/valyala/fasthttp/fasthttpproxy"
 	"io"
 	"strings"
 	"time"
 )
-
-type Client struct {
-	ft *fasthttp.Client
-}
 
 type Requests struct {
 	Url     string
@@ -21,7 +18,8 @@ type Requests struct {
 	Body    []byte
 	Timeout int
 	Retry   int
-	Proxy   string
+	// Requests配置代理 单独为请求配置代理 (随机代理或需要随时切换代理时使用)
+	Proxy string
 }
 
 type Response struct {
@@ -31,20 +29,46 @@ type Response struct {
 	Status  int
 }
 
-func NewClient() Client {
-	config := &fasthttp.Client{
-		TLSConfig:                 &tls.Config{InsecureSkipVerify: true},
-		MaxConnsPerHost:           1024,
-		ReadTimeout:               time.Duration(20) * time.Second,
-		WriteTimeout:              time.Duration(3) * time.Second,
-		NoDefaultUserAgentHeader:  true,
-		MaxIdemponentCallAttempts: 1,
-	}
-	return Client{ft: config}
+type Client struct {
+	SSLVerify                bool
+	MaxConnsPerHost          int
+	ReadTimeout              int
+	WriteTimeout             int
+	NoDefaultUserAgentHeader bool
+	// Client配置代理 全局使用 (动态代理或无需切换代理时使用)
+	Proxy string
 }
 
+// HTTPX Client默认配置
+var FT = &fasthttp.Client{
+	TLSConfig:                 &tls.Config{InsecureSkipVerify: true},
+	MaxConnsPerHost:           1024,
+	ReadTimeout:               time.Duration(20) * time.Second,
+	WriteTimeout:              time.Duration(3) * time.Second,
+	NoDefaultUserAgentHeader:  true,
+	MaxIdemponentCallAttempts: 1,
+}
+
+// 初始化Client自定义配置
+func InitClient(config *Client) {
+	FT.TLSConfig = &tls.Config{InsecureSkipVerify: !config.SSLVerify}
+	FT.MaxConnsPerHost = config.MaxConnsPerHost
+	FT.ReadTimeout = time.Duration(config.ReadTimeout) * time.Second
+	FT.WriteTimeout = time.Duration(config.WriteTimeout) * time.Second
+	FT.NoDefaultUserAgentHeader = config.NoDefaultUserAgentHeader
+
+	if len(strings.TrimSpace(config.Proxy)) > 0 {
+		if strings.HasPrefix(config.Proxy, "socks4://") || strings.HasPrefix(config.Proxy, "socks5://") {
+			FT.Dial = fasthttpproxy.FasthttpSocksDialer(config.Proxy)
+		} else {
+			FT.Dial = fasthttpproxy.FasthttpHTTPDialer(config.Proxy)
+		}
+	}
+}
+
+// 创建request
 func NewRequest() *Requests {
-	// 初始化基本的配置
+	// 初始化默认配置
 	return &Requests{
 		Headers: map[string]string{"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0"},
 		Timeout: 60,
@@ -52,39 +76,40 @@ func NewRequest() *Requests {
 	}
 }
 
-func (c *Client) Request(r *Requests) (*Response, error) {
-	if len(strings.TrimSpace(r.Proxy)) > 0 {
-		if strings.HasPrefix(r.Proxy, "socks4://") || strings.HasPrefix(r.Proxy, "socks5://") {
-			c.ft.Dial = fasthttpproxy.FasthttpSocksDialer(r.Proxy)
+func (request *Requests) Request() (*Response, error) {
+	fmt.Println(FT)
+	if len(strings.TrimSpace(request.Proxy)) > 0 {
+		if strings.HasPrefix(request.Proxy, "socks4://") || strings.HasPrefix(request.Proxy, "socks5://") {
+			FT.Dial = fasthttpproxy.FasthttpSocksDialer(request.Proxy)
 		} else {
-			c.ft.Dial = fasthttpproxy.FasthttpHTTPDialer(r.Proxy)
+			FT.Dial = fasthttpproxy.FasthttpHTTPDialer(request.Proxy)
 		}
 	}
 
 	fastReq := fasthttp.AcquireRequest()
 	defer fasthttp.ReleaseRequest(fastReq)
 
-	fastReq.Header.SetMethod(r.Method)
-	fastReq.SetRequestURI(r.Url)
+	fastReq.Header.SetMethod(request.Method)
+	fastReq.SetRequestURI(request.Url)
 
-	if len(r.Cookies) > 0 {
-		for k, v := range r.Cookies {
+	if len(request.Cookies) > 0 {
+		for k, v := range request.Cookies {
 			fastReq.Header.SetCookie(k, v)
 		}
 	}
 
-	fastReq.SetBody(r.Body)
+	fastReq.SetBody(request.Body)
 
 	fastResp := fasthttp.AcquireResponse()
 	defer fasthttp.ReleaseResponse(fastResp)
 
 	attempts := 0
 	for {
-		err := c.ft.DoTimeout(fastReq, fastResp, time.Duration(r.Timeout)*time.Second)
+		err := FT.DoTimeout(fastReq, fastResp, time.Duration(request.Timeout)*time.Second)
 		if err == nil || !isIdempotent(fastReq) && err != io.EOF {
 			break
 		}
-		if attempts >= r.Retry {
+		if attempts >= request.Retry {
 			return nil, err
 		}
 		attempts++
