4ec3fb8e662eeb6835769b4e7cdb6b3b7b5828d5
V <Vsir@88.com>
2022-11-22 17:56:16 +0700

    修改目录结构 导入模块无需重命名

---
+++ httpx_test.go
@@ -0,0 +1,26 @@
+package httpx
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGet(t *testing.T) {
+	config := &Client{
+		SSLVerify: true,
+		Proxy:     "192.168.1.248:8080",
+	}
+	// 使用自定义配置初始化Client
+	InitClient(config)
+
+	req := NewRequest()
+	req.Url = "http://www.google.com"
+	req.Method = GET
+	//req.Proxy = "192.168.1.248:8080"
+	resp, err := req.Request()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(string(resp.Headers["Server"]))
+	}
+}
